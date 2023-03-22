b0a2ea2c37266569adac8916711daba7fed8f108
W01000 <externalplay4702@gmail.com>
2023-03-22 18:40:03 -0300

    "second commit"

--- client/script.js
+++ client/script.js
@@ -73,7 +73,7 @@ const handleSubmit = async (e) => {
 
   loader(messageDiv);
 
-  const response = await fetch('http://localhost:8080', {
+  const response = await fetch('https://daffyccode-gpt.onrender.com', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
